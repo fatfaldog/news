fe7aa64f3e422fa437644b179caba21d3202d44e
aleksey <av.pavlow@gmail.com>
2021-11-01 13:53:15 +0300

    Delete

--- app/Services/API/Article/ArticleService.php
+++ app/Services/API/Article/ArticleService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\API\Article;
 
+use App\Models\Article;
 use App\Models\News;
 use App\Models\Category;
 use App\Services\API\Article\Source\Input\ArticleApiInput;
@@ -25,12 +26,16 @@ class ArticleService implements ArticlesInterface
     /**
      * Update Article from API
      * @return mixed
+     * @throws \Exception
      */
     public function handle()
     {
         $date = date('Y-m-d');
 
         $categories = Category::all();
+
+        Article::query()->delete();
+
         foreach ($categories as $category) {
             $result = $this->input->load($category->name, $date, $date, 'popularity');
 
